6fe3e79f7f519738684b1b2513358400d04f75d1
Felipe <figueiroa.fe@gmail.com>
2023-04-13 20:05:40 -0300

    Últimas configurações do ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,6 +26,23 @@ module.exports = {
     '@typescript-eslint',
   ],
   rules: {
-    "react/jsx-filename-extension": [1, { "extensions": [".tsx"] }]
+    "react/jsx-filename-extension": [1, { "extensions": [".tsx"] }],
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        "js": "never",
+        "jsx": "never",
+        "ts": "never",
+        "tsx": "never"
+      }
+    ]
   },
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"]
+      }
+    }
+  }
 };
