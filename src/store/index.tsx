629120c7e780a9bf523c4e2feb20041c30444d63
Felipe <figueiroa.fe@gmail.com>
2023-04-20 15:19:36 -0300

    Atualização da interface de User e definição da ActionTypes para as requisições de user

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,12 +1,49 @@
 import React, { createContext, useContext, useReducer } from 'react';
 
 interface User {
-  id: string;
+  id?: string;
+  created_at?: string;
+  updated_at?: string;
   email: string;
   first_name: string;
   last_name: string;
-  onboard_complete: string;
-  group: string[];
+  nickname?: string;
+  bio?: string;
+  birthday?: string;
+  children_qty?: number;
+  civil_state?: string;
+  instagram?: string;
+  whats_app?: string;
+  linkedin?: string;
+  city_current_id?: string;
+  city_born_at_id?: string;
+  state_current_id?: string;
+  state_born_at_id?: string;
+  company_id?: string;
+  occupation_id?: string;
+  work_field_id?: string;
+  skills_want?: string[];
+  skills_can?: string[];
+  hobbies?: string[];
+  onboard_complete?: boolean;
+  last_access_at?: string;
+  group?: string[];
+  spotify?: {
+    display_name?: string;
+    top_artist?: {
+      id?: string;
+      name?: string;
+      url?: string;
+      genres?: string[];
+    };
+    top_genre?: string;
+    most_recent_saved_show?: {
+      id?: string;
+      name?: string;
+      url?: string;
+    };
+  };
+  badge?: string;
 }
 interface State {
   data?: any;
@@ -32,6 +69,14 @@ const ActionType = {
   CONFIRM_EMAIL_START: 'CONFIRM_EMAIL_START',
   CONFIRM_EMAIL_SUCCESS: 'CONFIRM_EMAIL_SUCCESS',
   CONFIRM_EMAIL_ERROR: 'CONFIRM_EMAIL_ERROR',
+  // USER
+  USER_START: 'USER_START',
+  USER_SUCCESS: 'USER_SUCCESS',
+  USER_ERROR: 'USER_ERROR',
+  // CREATE USER
+  CREATE_USER_START: 'CREATE_USER_START',
+  CREATE_USER_SUCCESS: 'CREATE_USER_SUCCESS',
+  CREATE_USER_ERROR: 'CREATE_USER_ERROR',
 };
 
 type Action = { type: string; payload?: any };
@@ -110,6 +155,49 @@ const reducer = (state: State, action: Action) => {
       };
 
     case ActionType.CONFIRM_EMAIL_ERROR:
+      return {
+        ...state,
+        loading: false,
+        error: true,
+      };
+
+    case ActionType.USER_START:
+      return {
+        ...state,
+        loading: true,
+        error: false,
+      };
+
+    case ActionType.USER_SUCCESS:
+      return {
+        ...state,
+        user: action.payload,
+        loading: false,
+        error: false,
+      };
+
+    case ActionType.USER_ERROR:
+      return {
+        ...state,
+        loading: false,
+        error: true,
+      };
+
+    case ActionType.CREATE_USER_START:
+      return {
+        ...state,
+        loading: true,
+        error: false,
+      };
+
+    case ActionType.CREATE_USER_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        error: false,
+      };
+
+    case ActionType.CREATE_USER_ERROR:
       return {
         ...state,
         loading: false,
